aa379447a2fc67970e3a4bae6238fe396f6f81c1
Dev Verma <dev.verma@joshtechnologygroup.com>
2026-08-04 00:34:09 +0530

    [DV_A3_01]: Initial commit

---
+++ Assignment3/Models/Restaurant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.UI.WebControls;
+
+namespace Assignment3.Models
+{
+    [Table("Restaurants")]
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [Required]
+        public long AddressId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        [ForeignKey("AddressId")]
+        public virtual Address Address { get; set; }
+
+        public virtual ICollection<MenuItem> MenuItems { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+        public virtual ICollection<RestaurantOwner> RestaurantOwners { get; set; }
+
+        public Restaurant()
+        {
+            MenuItems = new HashSet<MenuItem>();
+            Orders = new HashSet<Order>();
+            RestaurantOwners = new HashSet<RestaurantOwner>();
+        }
+    }
+}
---
+++ Assignment3/Models/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Assignment3.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Role { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        // Navigation Property
+        public virtual ICollection<Address> Addresses { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+        public virtual ICollection<RestaurantOwner> RestaurantOwners { get; set; }
+
+        public virtual ICollection<RefreshToken> RefreshTokens { get; set; }
+
+        public User()
+        {
+            Addresses = new HashSet<Address>();
+            Orders = new HashSet<Order>();
+            RestaurantOwners = new HashSet<RestaurantOwner>();
+            RefreshTokens = new HashSet<RefreshToken>();
+        }
+    }
+}
